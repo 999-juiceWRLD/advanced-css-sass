841e973d058b64cbe5766d2a0ced025d56e21e96
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-22 21:46:45 +0300

    create `<HeaderButton />` component for the button

---
+++ src/component/header-component/header-button/header-button.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header-button.styles.css";
+
+function HeaderButton() {
+    return (
+        <button type="button" className="btn btn-white">
+            <a href="#" id="btn-a">Discover Our Tools</a>
+        </button>
+    );
+}
+
+export default HeaderButton;
