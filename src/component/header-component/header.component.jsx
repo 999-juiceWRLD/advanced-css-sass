00860919659ef3c9f10c412b787c6566a71d4e27
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-22 15:26:23 +0300

    create `<HeaderComp />` component

---
+++ src/component/header-component/header.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HeaderComp() {
+    return (
+        <header className="header">
+            some text...
+        </header>
+    );
+}
+
+export default HeaderComp;
