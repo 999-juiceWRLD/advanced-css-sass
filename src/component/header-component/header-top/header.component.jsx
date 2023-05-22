49f489b224094c76bde0694dff44bb20df098a71
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-22 21:49:58 +0300

    add `<HeaderLogo />` to `<HeaderComp />`

--- src/component/header-component/header-top/header.component.jsx
+++ src/component/header-component/header-top/header.component.jsx
@@ -3,16 +3,12 @@ import "./header-component.styles.css";
 import "../../../assets/css/style.css";
 import HeaderText from '../header-text/header-text.component';
 import HeaderButton from "../header-button/header-button.component";
+import HeaderLogo from "../header-logo/header-logo.component";
 
 function HeaderComp() {
     return (
         <header className="header">
-            <div className="logo-box">
-                <img src={require("../../../assets/img/logo-white.png")}
-                     alt="logo"
-                     className="logo" 
-                />
-            </div>
+            <HeaderLogo />
             <HeaderText />
             <HeaderButton />
         </header>
