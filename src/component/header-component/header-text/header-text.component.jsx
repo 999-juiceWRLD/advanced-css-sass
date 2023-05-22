c5b5a0f35a20951a2b652b49c58147b675701b2f
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-22 17:42:27 +0300

    create `header-text` folder

---
+++ src/component/header-component/header-text/header-text.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HeaderText() {
+    return (
+        <div className="heading-primary">
+            <span className="heading-primary-main">Outdoors</span>
+            <span className="heading-primary-sub">
+                is where life happens
+            </span>
+        </div>
+    );
+}
+
+export default HeaderText;
