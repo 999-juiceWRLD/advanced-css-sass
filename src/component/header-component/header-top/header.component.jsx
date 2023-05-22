bcc9ed5a9dda9128fee608b65fb9b3df629dadd7
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-22 21:31:10 +0300

    add `<HeaderButton />` inside the `<HeaderComp />`

--- src/component/header-component/header-top/header.component.jsx
+++ src/component/header-component/header-top/header.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./header-component.styles.css";
 import "../../../assets/css/style.css";
 import HeaderText from '../header-text/header-text.component';
-
+import HeaderButton from "../header-button/header-button.component";
 
 function HeaderComp() {
     return (
@@ -14,6 +14,7 @@ function HeaderComp() {
                 />
             </div>
             <HeaderText />
+            <HeaderButton />
         </header>
     );
 }
