4ed502f42f8f08bdb24d565f3c603ba102762ba9
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-23 00:25:11 +0300

    remove `<HeaderLogo />`

--- src/component/header-component/header-top/header.component.jsx
+++ src/component/header-component/header-top/header.component.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./header-component.styles.css";
 import "../../../assets/css/style.css";
 import HeaderText from '../header-text/header-text.component';
-import HeaderButton from "../header-button/header-button.component";
 import HeaderLogo from "../header-logo/header-logo.component";
 
 function HeaderComp() {
@@ -10,7 +9,6 @@ function HeaderComp() {
         <header className="header">
             <HeaderLogo />
             <HeaderText />
-            <HeaderButton />
         </header>
     );
 }
