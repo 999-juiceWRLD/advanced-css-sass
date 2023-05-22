bc38b5ee6c1b44d69a651f00f938a05b6ec4005b
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-23 00:27:14 +0300

    add `<HeaderButton />`

--- src/component/header-component/header-text/header-text.component.jsx
+++ src/component/header-component/header-text/header-text.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./header-text.styles.css";
+import HeaderButton from "../header-button/header-button.component";
 
 function HeaderText() {
     return (
@@ -10,6 +11,7 @@ function HeaderText() {
                     is where life happens
                 </span>
             </div>
+            <HeaderButton />
         </div>
     );
 }
