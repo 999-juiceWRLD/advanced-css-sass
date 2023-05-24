c1091977eb8a90a20a6ddda05f8e5a87cd366c59
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-25 00:43:23 +0300

    add BEM styling to `header-text/`

--- src/component/header-component/header-text/header-text.component.jsx
+++ src/component/header-component/header-text/header-text.component.jsx
@@ -4,10 +4,10 @@ import HeaderButton from "../header-button/header-button.component";
 
 function HeaderText() {
     return (
-        <div className="text-box">
+        <div className="header__text-box">
             <div className="heading-primary">
-                <span className="heading-primary-main">Outdoors</span>
-                <span className="heading-primary-sub">
+                <span className="heading-primary--main">Outdoors</span>
+                <span className="heading-primary--sub">
                     is where life happens
                 </span>
             </div>
