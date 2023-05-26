a877d1e2ea21abf3e609fb4ce9b964cdc5fbc7a8
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-27 01:19:53 +0300

    this is going to be the joint page of side by side text and image

---
+++ src/component/main-body-component/text-img-composition/text-img.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./text-img.styles.scss";
+
+function TextImgRow() {
+    return (
+        <div className="row">
+            <div className="col-element">text</div>
+            <div className="col-element">image comp</div>
+        </div>
+    );
+}
+
+export default TextImgRow;
