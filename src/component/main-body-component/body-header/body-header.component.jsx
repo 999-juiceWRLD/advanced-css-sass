2d4b1ffc638d70a49922752bb1a168a9e97e8ccf
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-27 00:58:04 +0300

    create this folder for body heading

---
+++ src/component/main-body-component/body-header/body-header.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./body-header.styles.scss";
+
+function BodyHeadingText() {
+    return (
+        <div className="u-center-text">
+            <h2 className="heading-secondary">
+                Exciting Tours for Adventurous People
+            </h2>
+        </div>
+        
+    );
+}
+
+export default BodyHeadingText;
