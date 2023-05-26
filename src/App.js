6c771d9db75de91ea8f6de5aa6ea682d69df8a50
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-27 00:09:59 +0300

    add `<Body />` component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HeaderComp from './component/header-component/header-top/header.component';
+import Body from './component/main-body-component/main-body/body.component';
 
 function App() {
   return (
     <div className="App">
       <HeaderComp />
+      <Body />
     </div>
   );
 }
