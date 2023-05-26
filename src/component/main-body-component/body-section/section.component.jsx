83a3a00ac5e29453a1eb34473cf0fc8072d7b6e4
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-27 01:05:50 +0300

    include `TextImgRow />`

--- src/component/main-body-component/body-section/section.component.jsx
+++ src/component/main-body-component/body-section/section.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./section.styles.scss";
 import BodyHeadingText from "../body-header/body-header.component";
+import TextImgRow from "../text-img-composition/text-img.component";
 
 function BodySection() {
     return (
         <section className="section-about">
             <BodyHeadingText />
+            <TextImgRow />
         </section>
     );
 }
