e067f2db80907fd1b091033a74027cae87c9e2e0
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-26 23:58:54 +0300

    start building body

---
+++ src/component/main-body-component/body-section/section.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BodySection() {
+    return (
+        <section className="section-about">
+                
+        </section>
+    );
+}
+
+export default BodySection;
---
+++ src/component/main-body-component/main-body/body.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Body() {
+    return (
+        <main></main>
+    );
+}
+
+export default Body;
