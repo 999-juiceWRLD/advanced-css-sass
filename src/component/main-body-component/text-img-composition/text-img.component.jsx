d6b28562a0c29bda38366c77ca82ca4b88b6b2db
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-27 02:06:53 +0300

    add text, img will be put here soon

--- src/component/main-body-component/text-img-composition/text-img.component.jsx
+++ src/component/main-body-component/text-img-composition/text-img.component.jsx
@@ -4,8 +4,35 @@ import "./text-img.styles.scss";
 function TextImgRow() {
     return (
         <div className="row">
-            <div className="col-element">text</div>
-            <div className="col-element">image comp</div>
+            <div className="col-element">
+                <h3 className="heading-tertiary heading-margin">
+                    You're going to fall in love with nature
+                </h3>
+                <p className="paragraph">
+                    Green and blue are the life. This is not a trip that you'd regret. 
+                    In nature, you will find not yourself, but also humankind's ancestors.
+                    Be amazed by the flamboyant, elegant animals. Who wouldn't want to 
+                    make it there?
+                </p>
+
+                <h3 className="heading-tertiary heading-margin">
+                    Live Adventures Like You Never Had Before
+                </h3>
+                <p className="paragraph">
+                    This, is going to be your most exciting journey. Animals in the wild life
+                    is going to amaze you, therefore you will be the one who collects memories.
+                    It will worth, we promise. 
+                </p>
+
+            </div>
+            <div className="col-element">
+                <h3 className="heading-tertiary">
+                    You're going to fall in love with nature
+                </h3>
+                <p className="paragraph">
+                    This, is going to be your most exciting journey. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum adipisci magni aperiam sunt voluptatem doloribus velit dolor ex eaque autem ut est consequatur porro doloremque minus quasi dolore, dignissimos quam.
+                </p>
+            </div>
         </div>
     );
 }
