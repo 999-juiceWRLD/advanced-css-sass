aca26128547bc3ca5ec3c84e115dc04d6bd34bb2
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-28 03:08:33 +0300

    update imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import HeaderComp from './component/header-component/header-top/header.component';
-import Body from './component/main-body-component/main-body/body.component';
+import Body from './component/about-component/about-body/about-body.component';
 
 function App() {
   return (
