c8486f826c12a3a26d654c608f62fa4336dc2b07
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-28 13:35:14 +0300

    create a prop called `props.icon` for core, where this will be replaced with relevant icons for each card

--- src/component/features-component/feature-core/feature-core.component.jsx
+++ src/component/features-component/feature-core/feature-core.component.jsx
@@ -4,7 +4,7 @@ import "./feature-core.styles.scss";
 function FeatureCore(props) {
     return (
         <div className="feature-box">
-                <i className="feature-box__icon icon-basic-world"></i>
+                <i className={`feature-box__icon ${props.icon}`}></i>
                 <h3 className="heading-tertiary">{props.heading}</h3>
                 <p className="feature-box__text">{props.text}</p>
         </div>
