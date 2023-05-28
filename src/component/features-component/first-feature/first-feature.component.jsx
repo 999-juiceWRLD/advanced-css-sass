621aa412d7b08d05cbc6af99ffb63e6e2dc0ed7a
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-28 11:56:36 +0300

    add first features folder

---
+++ src/component/features-component/first-feature/first-feature.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./first-feature.styles.scss";
+import FeatureCore from "../feature-core/feature-core.component";
+
+function FirstFeature() {
+    return (
+        <div className="col-1-of-4">
+            <FeatureCore
+                heading={"Explore The World"}
+                text={"Lorem ipsum falan filan. bize paranı kazandır \
+                      sonra siktir git nereye gidersen."} 
+            />
+        </div>
+    );
+}
+
+export default FirstFeature;
