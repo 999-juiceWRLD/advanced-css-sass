9b74ac1f26b0b6b4f1f4719efc623266f4db86f5
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-28 11:53:37 +0300

    update imports, use `FirstFeature />` inside the `<Feature />` component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import HeaderComp from './component/header-component/header-top/header.component';
 import Body from './component/about-component/about-body/about-body.component';
 import FirstFeature from './component/features-component/first-feature/first-feature.component';
+import Feature from './component/features-component/features-section.component';
 
 function App() {
   return (
     <div className="App">
       <HeaderComp />
       <Body />
-      <FirstFeature />
+      <Feature 
+        childComponent1={<FirstFeature />}
+      />
+      {/* <FirstFeature /> */}
     </div>
   );
 }
