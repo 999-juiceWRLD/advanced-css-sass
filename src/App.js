c6da48465a83269725d3c83cd951051df70812c3
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-28 11:37:10 +0300

    add features component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HeaderComp from './component/header-component/header-top/header.component';
 import Body from './component/about-component/about-body/about-body.component';
+import FirstFeature from './component/features-component/first-feature/first-feature.component';
 
 function App() {
   return (
     <div className="App">
       <HeaderComp />
       <Body />
+      <FirstFeature />
     </div>
   );
 }
