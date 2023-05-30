820a8aa162db97494147a0aa66d399baa5bae064
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-31 01:51:37 +0300

    create second and third components functions

---
+++ src/component/tours-component/tours-row/second-component/second-comp.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SecondComponent() {
+    return (
+    <div className="col-2">
+        col 2
+    </div>
+    );
+}
+
+export default SecondComponent;
---
+++ src/component/tours-component/tours-row/third-component/third-comp.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ThirdComponent() {
+    return (
+        <div className="col-3">
+            col 3
+        </div>
+    );
+}
+
+export default ThirdComponent;
