62a612111e3567d4b90b27e3752d739a5c3204d5
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-31 00:23:23 +0300

    add rest components, make it complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import HeaderComp from './component/header-component/header-top/header.component';
 import Body from './component/about-component/about-body/about-body.component';
 import FirstFeature from './component/features-component/first-feature/first-feature.component';
+import SecondFeature from './component/features-component/second-feature/second-feature.component';
+import ThirdFeature from './component/features-component/third-feature/third-feature.component';
+import FourthFeature from './component/features-component/fourth-feature/fourth-feature.component';
 import Feature from './component/features-component/feature-section/features-section.component';
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
       <Body />
       <Feature 
         childComponent1={<FirstFeature />}
+        childComponent2={<SecondFeature />}
+        childComponent3={<ThirdFeature />}
+        childComponent4={<FourthFeature />}
       />
     </div>
   );
