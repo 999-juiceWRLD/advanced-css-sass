11a9101318e6921b15b06829a02c2e54b4fa3498
juice wrld <ankaraunifedaisi@gmail.com>
2023-06-01 01:15:52 +0300

    this is the main board of the registering part

---
+++ src/component/booking-component/main/main-board.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./main-board.styles.scss";
+
+function MainBoard() {
+    return (
+        <section className="section-book">
+            <div className="row">
+                <div className="book">
+                    
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default MainBoard;
