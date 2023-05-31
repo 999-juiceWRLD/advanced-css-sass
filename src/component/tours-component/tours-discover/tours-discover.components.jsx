c6f635e0bbd03df961f781c91b3c746d8aeacc47
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-31 13:59:56 +0300

    change margin from big to huge

--- src/component/tours-component/tours-discover/tours-discover.components.jsx
+++ src/component/tours-component/tours-discover/tours-discover.components.jsx
@@ -3,7 +3,7 @@ import "./tours-discover.styles.scss";
 
 function DiscoverButton() {
     return (
-        <a href="#" className="btn btn--green u-margin-top-big">
+        <a href="#" className="btn btn--green u-margin-top-huge">
             Discover All Tours
         </a>
     );
