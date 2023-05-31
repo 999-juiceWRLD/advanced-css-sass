1b2c4d4924ef454d9662ac69076a3e27bbf0df3a
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-31 16:10:21 +0300

    create `<img />` tag

--- src/component/stories-component/figure-details/figures.component.jsx
+++ src/component/stories-component/figure-details/figures.component.jsx
@@ -4,7 +4,9 @@ import "./figures.styles.scss";
 function Figure(props) {
     return (
         <figure className="story__shape">
-            
+            <img src={require("../../../assets/img/nat-8.jpg")} 
+                 className="story__img"
+                 alt="client on a tour" />
         </figure>
     );
 }
