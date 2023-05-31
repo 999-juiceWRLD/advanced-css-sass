220d204521e8d1f55ee8d33d9f732d056508c603
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-31 14:35:48 +0300

    this is the header for stories section

---
+++ src/component/stories-component/section-header/section-header.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./section-header.styles.scss";
+
+function SectionHeader() {
+    return (
+        <section className="section__stories">
+            <div className="u-center-text u-margin-bottom-big">
+                <h2 className="heading-secondary">
+                    We Make People Happy
+                </h2>
+            </div>
+        </section>
+    );
+}
+
+export default SectionHeader;
