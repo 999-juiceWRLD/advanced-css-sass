f13ad5339bef700a7899a09c9f8a10b2e80ce042
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-31 19:48:42 +0300

    add `<Figure />` and `<StoryText />` components with props

--- src/component/stories-component/story-base/base-story.component.jsx
+++ src/component/stories-component/story-base/base-story.component.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./base-story-comp.styles.scss";
 import Figure from "../figure-details/figures.component";
+import StoryText from "../story-text/story-text.component";
 
 function BaseStoryComp(props) {
     return (
         <div className="story">
-            <Figure />
-            {props.children}
+            <Figure path={props.path} 
+                    name={props.name}/>
+            <StoryText 
+                    title={props.title}
+                    text={props.text}/>
         </div>
     );
 }
