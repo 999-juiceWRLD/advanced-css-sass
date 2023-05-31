40385d1ca3ff454d1786f69c3eb9f13577cab11f
juice wrld <ankaraunifedaisi@gmail.com>
2023-06-01 00:27:08 +0300

    create stories button

---
+++ src/component/stories-component/stories-button/stories-button.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./stories-button.styles.scss";
+
+function StoriesButton() {
+    return (
+        <div className="plane u-center-text u-margin-top-huge">
+            <a href="#" className="button-text">
+                Read all stories &rarr;
+            </a>
+        </div>
+    );
+}
+
+export default StoriesButton;
