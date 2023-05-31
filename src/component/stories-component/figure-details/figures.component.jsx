e368269585317cfb4dec4db769694cc224bd9acb
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-31 15:40:00 +0300

    add figure folder

---
+++ src/component/stories-component/figure-details/figures.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./figures.styles.scss";
+
+function Figure(props) {
+    return (
+        <figure className="story__shape">
+            
+        </figure>
+    );
+}
+
+export default Figure;
