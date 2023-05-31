294531ab9c17b32f5e5459522093f2b9afc92118
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-31 13:48:22 +0300

    create discover bbutton

---
+++ src/component/tours-component/tours-discover/tours-discover.components.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DiscoverButton() {
+    return (
+        <a href="#" className="btn">
+            Discover All Tours
+        </a>
+    );
+}
+
+export default DiscoverButton;
