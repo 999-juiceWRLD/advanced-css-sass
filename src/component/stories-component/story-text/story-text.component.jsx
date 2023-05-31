20ade2aa2145399e299172cc4f86a1e37f6dfe92
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-31 14:39:14 +0300

    this is the text component that will show stories

---
+++ src/component/stories-component/story-text/story-text.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function StoryText(props) {
+    return (
+        <div className="story">
+            {props.text}
+        </div>
+    );
+}
+
+export default StoryText;
