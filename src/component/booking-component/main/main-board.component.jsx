3ff4df48b098e3af29c6e572d1ffe3363400ba8e
juice wrld <ankaraunifedaisi@gmail.com>
2023-06-01 18:10:38 +0300

    fix typo

--- src/component/booking-component/main/main-board.component.jsx
+++ src/component/booking-component/main/main-board.component.jsx
@@ -24,7 +24,7 @@ function MainBoard() {
                             <div className="form__group">
                                 <input type="text" className="form__input" id="email"
                                        placeholder="Email Address" required />
-                                <label htmlFor="email" className="form__label">Full Name</label>
+                                <label htmlFor="email" className="form__label">Email Address</label>
                             </div>
                         </form>
                     </div>
