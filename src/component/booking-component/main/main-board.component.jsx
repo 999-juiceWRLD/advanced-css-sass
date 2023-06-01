f70691c34bf2e9963b5fc3de12b40faee6a0c1b4
juice wrld <ankaraunifedaisi@gmail.com>
2023-06-01 14:13:32 +0300

    create transparent linear gradient, add a image-overlay div

--- src/component/booking-component/main/main-board.component.jsx
+++ src/component/booking-component/main/main-board.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./main-board.styles.scss";
+import forestImage from "../../../assets/img/nat-10.jpg";
 
 function MainBoard() {
     return (
         <section className="section-book">
             <div className="row">
                 <div className="book">
-                    
+                    <img alt="forest" src={forestImage} className="book-img" /> 
+                    <div className="image-overlay"></div>
                 </div>
             </div>
         </section>
