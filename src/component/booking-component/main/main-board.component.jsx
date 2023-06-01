3f3a45d54aaee1f9cbf68f1e027842c581cb48c1
juice wrld <ankaraunifedaisi@gmail.com>
2023-06-01 18:07:28 +0300

    add forms and styling

--- src/component/booking-component/main/main-board.component.jsx
+++ src/component/booking-component/main/main-board.component.jsx
@@ -11,10 +11,20 @@ function MainBoard() {
                     <div className="image-overlay"></div>
                     <div className="form-container">
                         <form action="#" className="form">
+                            <div className="u-center-text u-margin-bottom-big">
+                                <h2 className="heading-secondary">
+                                    Register Now
+                                </h2>
+                            </div>
                             <div className="form__group">
                                 <input type="text" className="form__input" id="name"
                                        placeholder="Full Name" required />
                                 <label htmlFor="name" className="form__label">Full Name</label>
+                            </div>
+                            <div className="form__group">
+                                <input type="text" className="form__input" id="email"
+                                       placeholder="Email Address" required />
+                                <label htmlFor="email" className="form__label">Full Name</label>
                             </div>
                         </form>
                     </div>
