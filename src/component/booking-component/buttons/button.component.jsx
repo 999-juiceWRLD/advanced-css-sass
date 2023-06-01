bbd79965b135b6dcc7aec2479f3ba0aeb1438fe2
juice wrld <ankaraunifedaisi@gmail.com>
2023-06-01 23:33:34 +0300

    add labels

--- src/component/booking-component/buttons/button.component.jsx
+++ src/component/booking-component/buttons/button.component.jsx
@@ -4,11 +4,11 @@ function Buttons() {
     return (
         <div className="form__group">
             <div className="form__radio-group">
-                <input type="radio" className="form__radio-input" id="small"/>
+                <input type="radio" className="form__radio-input" id="small" name="size"/>
                 <label htmlFor="small" className="form__radio-label">Small Tour Group</label>
             </div>
             <div className="form__radio-group">
-                <input type="radio" className="form__radio-input" id="large"/>
+                <input type="radio" className="form__radio-input" id="large" name="size"/>
                 <label htmlFor="large" className="form__radio-label">Large Tour Group</label>
             </div>
         </div>
