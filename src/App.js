0d822bee98a9ef49ebc0525af14ccd8cb9d607ba
juice wrld <ankaraunifedaisi@gmail.com>
2023-06-02 16:49:09 +0300

    import `<FooterMain />`

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SectionCore from './component/stories-component/section-core/section-core
 import BaseStoryComp from './component/stories-component/story-base/base-story.component';
 import StoriesButton from './component/stories-component/stories-button/stories-button.component';
 import MainBoard from './component/booking-component/main/main-board.component';
+import FooterMain from './component/footer-component/footer-main.component';
 
 function App() {
   return (
@@ -49,6 +50,7 @@ function App() {
       />
         <StoriesButton />
         <MainBoard />
+        <FooterMain />
     </div>
   );
 }
