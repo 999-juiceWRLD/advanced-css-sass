0e31d32febae3f857e1422a2fc4f2d3005e656eb
juice wrld <ankaraunifedaisi@gmail.com>
2023-06-04 23:48:28 +0300

    concatenate p tags and add background color

--- src/component/footer-component/footer-main/footer-main.component.jsx
+++ src/component/footer-component/footer-main/footer-main.component.jsx
@@ -10,15 +10,13 @@ function FooterMain() {
                 <img src={logoGreen} alt="" className="footer__logo" />
             </div>
             <div className="row">
-                <div className="col-1-of-2">
+                <div className="col-1-of-2" style={{ display: "flex" }} >
                     <FooterList />
                 </div>
                 <div className="col-2-of-2">
                     <p className="footer__copyright">
-                        Built by <a href="https://github.com/999-juiceWRLD" target="blank" className="footer__copyright">Kahraman CANIKOGLU</a>
-                    </p>
-                    <p className="footer__copyright">
-                    Designed by <a href="https://github.com/jonasschmedtmann" target="blank" className="footer__copyright">Jonas SCHMEDTMANN</a>
+                        Built by <a href="https://github.com/999-juiceWRLD" target="blank" className="footer__copyright">Kahraman CANIKOGLU</a> &
+                        Designed by <a href="https://github.com/jonasschmedtmann" target="blank" className="footer__copyright">Jonas SCHMEDTMANN</a>
                     </p>
                 </div>
             </div>
