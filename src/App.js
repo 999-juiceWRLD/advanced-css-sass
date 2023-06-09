ea460093dbcdbf55553e9c600b851d4f2bc32ca6
juice wrld <ankaraunifedaisi@gmail.com>
2023-06-09 03:08:26 +0300

    make this file ready for having custom components, remove others

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-// import './App.css';
+import './App.css'; 
 import "./assets/sass/main.scss";
 import "./assets/css/style.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>hi</p>
-      </header>
+      
     </div>
   );
 }
