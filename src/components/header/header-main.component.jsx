69baf3313558c631c9f6d74a4f380989e6e3c0c9
juice wrld <ankaraunifedaisi@gmail.com>
2023-06-10 12:17:07 +0300

    add header, form, div, nav tags

--- src/components/header/header-main.component.jsx
+++ src/components/header/header-main.component.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import logoImg from "../../assets/img/logo.png";
 
 function HeaderMain() {
     return (
         <div className="container">
             <header className="header">
-                Header Part
+                <img src={logoImg} alt="trillio-logo" className="logo" />
             </header>
+            <form action="#" className="search">
+                <input type="text" className="search__input" placeholder="Search Hotels" />
+                <button className="search__button">
+                    
+                </button>
+            </form>
             <div className="content">
                 <nav className="sidebar">
-                
+                    sidebar
                 </nav>
                 <main className="hotel-view">
-
+                    hotel    
                 </main>
             </div>
         </div>
