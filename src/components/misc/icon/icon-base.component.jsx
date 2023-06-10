8733c26bfb180927977991117467dbf8f05fc427
juice wrld <ankaraunifedaisi@gmail.com>
2023-06-11 01:20:52 +0300

    create `<Icon />` for re-usable icon components

---
+++ src/components/misc/icon/icon-base.component.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import symbolDef from "../../../assets/icons/symbol-defs.svg";
+
+function Icon({name, ...props}) {
+
+    const iconNames = [
+        "bookmark",
+        "chevron-thin-right",
+        "aircraft-take-off",
+        "chat",
+        "home",
+        "key",
+        "location-pin",
+        "magnifying-glass",
+        "map",
+        "star"
+    ]
+
+    if (!(iconNames.includes(name))) {
+        console.warn("no such icon with this name, maybe check again?");
+    }
+
+    const iconPath = `${symbolDef}#icon-${name}`;
+
+    return (
+        <svg>
+            <use xlinkHref={iconPath}></use>
+        </svg>
+    );
+}
+
+export default Icon;
