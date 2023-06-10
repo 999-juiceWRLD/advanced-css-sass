b7f67c53b35aead6eea4aea5307909d0cf706cd7
juice wrld <ankaraunifedaisi@gmail.com>
2023-06-11 01:23:52 +0300

    create `<HeaderMain />`

--- src/components/header/header-main.component.jsx
+++ src/components/header/header-main.component.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import logoImg from "../../assets/img/logo.png";
+import Icon from "../misc/icon/icon-base.component";
 
 function HeaderMain() {
     return (
         <div className="container">
             <header className="header">
                 <img src={logoImg} alt="trillio-logo" className="logo" />
+                <form action="#" className="search">
+                    <input type="text" className="search__input" placeholder="Search Hotels" />
+                    <button className="search__button">
+                        <Icon name="magnifying-glass" />
+                    </button>
+                </form>
+                <nav className="user-nav"></nav>
             </header>
-            <form action="#" className="search">
-                <input type="text" className="search__input" placeholder="Search Hotels" />
-                <button className="search__button">
-                    
-                </button>
-            </form>
             <div className="content">
                 <nav className="sidebar">
                     sidebar
