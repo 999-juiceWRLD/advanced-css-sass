5732480cd40a446047c61bb55a0c02cb5f581ad9
juice wrld <ankaraunifedaisi@gmail.com>
2023-06-19 22:36:04 +0300

    add navigation bar component

---
+++ src/components/navigation/navbar-component.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Icon from "../misc/icon/icon-base.component";
+import NavListElement from "../navigation-element/navlist-component";
+
+function NavigationBar() {
+    return (
+            <div className="content">
+                <nav className="sidebar">
+                    <NavListElement 
+                        iconName="home"
+                        spanName="Hotel"
+                    />
+                    <NavListElement 
+                        iconName="aircraft-take-off"
+                        spanName="Flight"
+                    />
+                    <NavListElement 
+                        iconName="key"
+                        spanName="Car Rental"
+                    />
+                    <NavListElement 
+                        iconName="map"
+                        spanName="Tours"
+                    />
+
+                    <div className="legal">
+                        <p>&copy; 2023 Trillio GmBh. All rights reserved.</p> 
+                    </div>
+                </nav>
+                <main className="hotel-view">
+                    hotel    
+                </main>
+            </div>
+    );
+}
+
+export default NavigationBar;
