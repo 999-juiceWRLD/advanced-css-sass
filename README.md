7353229806435fe147e287e25f9368ed88139019
juice wrld <ankaraunifedaisi@gmail.com>
2023-07-21 02:11:58 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nexter Project
+
+Instead of using just React, this time I wanted to use React with Typescript (`.tsx`).
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App: React.FunctionComponent = () => {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
