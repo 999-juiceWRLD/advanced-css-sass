ccec7235bf035bce5f4d0d882e38dc839c43c8b0
juice wrld <ankaraunifedaisi@gmail.com>
2023-07-22 16:27:03 +0300

    import relevant components and use it inside `<App />` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import Sidebar from "./components/sidebar/Sidebar.component"
+import Header from "./components/header/Header.component"
+import Realtor from "./components/realtor/Realtor.component"
+import Features from "./components/features/Features.component"
+import Story from "./components/story/Story.component"
+import Homes from "./components/homes/Homes.component"
+import Gallery from "./components/gallery/Gallery.component"
+import Footer from "./components/footer/Footer.component"
+
 const App: React.FunctionComponent = () => {
 
   return (
-    <div>
-      
+    <div className="container">
+      <Sidebar />
+      <Header />
+      <Realtor />
+      <Features />
+      <Story />
+      <Homes />
+      <Gallery />
+      <Footer />
     </div>
   )
 }
